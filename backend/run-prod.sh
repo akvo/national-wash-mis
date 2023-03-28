aed251d6df4b50ee95d114b72d016c900b3ea904
dedenbangkit <mail@dedenbangkit.com>
2023-03-28 16:02:13 +0700

    [#13] Debug server

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -6,19 +6,21 @@ set -eu
 python manage.py migrate
 python manage.py generate_config > /dev/null &
 
-function log {
-   echo "$(date +"%T") - START INFO - $*"
-}
+python manage.py runserver 0.0.0.0:8000
 
-_term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$child" 2>/dev/null
-}
-
-trap _term SIGTERM
-
-log Starting gunicorn in background
-gunicorn nwmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
-
-child=$!
-wait "$child"
+# function log {
+#    echo "$(date +"%T") - START INFO - $*"
+# }
+#
+# _term() {
+#   echo "Caught SIGTERM signal!"
+#   kill -TERM "$child" 2>/dev/null
+# }
+#
+# trap _term SIGTERM
+#
+# log Starting gunicorn in background
+# gunicorn nwmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
+#
+# child=$!
+# wait "$child"
