b73a95e1b6ca9baa1ac7ced0a3dc31c41efa3fc9
dedenbangkit <mail@dedenbangkit.com>
2023-03-28 16:05:59 +0700

    [#13] Create separate log file for access and error

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -6,21 +6,21 @@ set -eu
 python manage.py migrate
 python manage.py generate_config > /dev/null &
 
-python manage.py runserver 0.0.0.0:8000
+function log {
+   echo "$(date +"%T") - START INFO - $*"
+}
 
-# function log {
-#    echo "$(date +"%T") - START INFO - $*"
-# }
-#
-# _term() {
-#   echo "Caught SIGTERM signal!"
-#   kill -TERM "$child" 2>/dev/null
-# }
-#
-# trap _term SIGTERM
-#
-# log Starting gunicorn in background
-# gunicorn nwmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
-#
-# child=$!
-# wait "$child"
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+log Starting gunicorn in background
+gunicorn nwmis.wsgi --workers 6 --timeout 300 \
+	--bind 0.0.0.0:8000 \
+	--access-logfile ./access.log --error-logfile ./error.log &
+
+child=$!
+wait "$child"
