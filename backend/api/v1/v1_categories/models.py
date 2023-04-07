5ff003017d453ca5dad988dbf9b8721a3176d402
dedenbangkit <mail@dedenbangkit.com>
2023-04-07 11:34:38 +0700

    [#23] Data category initial migrations and models

---
+++ backend/api/v1/v1_categories/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from api.v1.v1_forms.models import Forms
+from api.v1.v1_data.models import FormData
+
+
+class DataCategory(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    data = models.ForeignKey(to=FormData,
+                             on_delete=models.DO_NOTHING,
+                             related_name='data_view_data_category')
+    form = models.ForeignKey(to=Forms,
+                             on_delete=models.DO_NOTHING,
+                             related_name='form_view_data_category')
+    options = models.JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'data_category'
