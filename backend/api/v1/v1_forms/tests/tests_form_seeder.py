47320d50273a30ec4668197970313e5eb815e938
ifirmawan <firmawaneiwan@gmail.com>
2023-04-10 12:14:47 +0700

    [#24] Update form seeder test to align w/ HCF

--- backend/api/v1/v1_forms/tests/tests_form_seeder.py
+++ backend/api/v1/v1_forms/tests/tests_form_seeder.py
@@ -49,14 +49,15 @@ class FormSeederTestCase(TestCase):
             "CLTS",
             "WASH in Schools",
             "Water System",
-            "Household questionnaire"
+            "Household questionnaire",
+            "Health facility questionnaire"
         ]
 
         # RUN SEED NEW FORM
         output = self.call_command()
         output = list(filter(lambda x: len(x), output.split("\n")))
         forms = Forms.objects.all()
-        self.assertEqual(forms.count(), 8)
+        self.assertEqual(forms.count(), 9)
         for form in forms:
             self.assertIn(f"Form Created | {form.name} V{form.version}",
                           output)
