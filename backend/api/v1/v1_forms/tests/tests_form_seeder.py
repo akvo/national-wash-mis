32eb9274fbf872a5ec1a4dadf8fe82679be8cb57
ifirmawan <firmawaneiwan@gmail.com>
2023-04-10 15:51:42 +0700

    [#24] Update form seeder test to align w/ school

--- backend/api/v1/v1_forms/tests/tests_form_seeder.py
+++ backend/api/v1/v1_forms/tests/tests_form_seeder.py
@@ -50,14 +50,15 @@ class FormSeederTestCase(TestCase):
             "WASH in Schools",
             "Water System",
             "Household questionnaire",
-            "Health facility questionnaire"
+            "Health facility questionnaire",
+            "School questionnaire",
         ]
 
         # RUN SEED NEW FORM
         output = self.call_command()
         output = list(filter(lambda x: len(x), output.split("\n")))
         forms = Forms.objects.all()
-        self.assertEqual(forms.count(), 9)
+        self.assertEqual(forms.count(), 10)
         for form in forms:
             self.assertIn(f"Form Created | {form.name} V{form.version}",
                           output)
