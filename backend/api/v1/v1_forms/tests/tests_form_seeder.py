e20895d51cdb2f1dd8a9e0fc1de21520e98bdb63
ifirmawan <firmawaneiwan@gmail.com>
2023-04-11 11:39:31 +0700

    [#24] Update form seeder test to align w/ Waterpoint

--- backend/api/v1/v1_forms/tests/tests_form_seeder.py
+++ backend/api/v1/v1_forms/tests/tests_form_seeder.py
@@ -53,13 +53,14 @@ class FormSeederTestCase(TestCase):
             "Health facility questionnaire",
             "School questionnaire",
             "CLTS progress tracking",
+            "Water Points questionnaire",
         ]
 
         # RUN SEED NEW FORM
         output = self.call_command()
         output = list(filter(lambda x: len(x), output.split("\n")))
         forms = Forms.objects.all()
-        self.assertEqual(forms.count(), 11)
+        self.assertEqual(forms.count(), 12)
         for form in forms:
             self.assertIn(f"Form Created | {form.name} V{form.version}",
                           output)
