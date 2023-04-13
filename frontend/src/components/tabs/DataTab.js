461a186d3d858067536fed4ab562cb1e8b8bb5d1
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-04-13 13:14:23 +0700

    [#19] Implementing i18n in tabs components

--- frontend/src/components/tabs/DataTab.js
+++ frontend/src/components/tabs/DataTab.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Tabs } from "antd";
 import { useNavigate } from "react-router-dom";
+import { store } from "../../lib";
+import { getTranslation } from "../../util";
 
 const { TabPane } = Tabs;
 
 const UserTab = ({ tabBarExtraContent }) => {
   const pathname = window.location.pathname;
   const navigate = useNavigate();
-
+  const { active: activeLang } = store.useState((s) => s.language);
+  const text = getTranslation(activeLang, "tabs");
   return (
     <Tabs
       size="large"
@@ -15,10 +18,10 @@ const UserTab = ({ tabBarExtraContent }) => {
       onChange={(key) => navigate(key)}
       tabBarExtraContent={tabBarExtraContent}
     >
-      <TabPane tab="Manage Data" key="/data/manage">
+      <TabPane tab={text.manageData} key="/data/manage">
         &nbsp;
       </TabPane>
-      <TabPane tab="Downloaded Data" key="/data/export">
+      <TabPane tab={text.downloadedData} key="/data/export">
         &nbsp;
       </TabPane>
     </Tabs>
