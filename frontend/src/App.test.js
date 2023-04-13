4b8e11842df833de69168dd6fc03dcc6fb36b1c7
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-04-13 14:15:49 +0700

    [#19] Update app test by adding window translation

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,24 @@ import { render, screen } from "@testing-library/react";
 import TestApp from "./TestApp";
 import "@testing-library/jest-dom";
 
+beforeAll(() => {
+  window.translations = {
+    en: {
+      page: {
+        header: {
+          signOut: "Sign Out",
+          dashboards: "Dashboards",
+          reports: "Reports",
+          newsEvents: "News & Events",
+          login: "Log in",
+          newDashboard: "New Dashboard",
+          households: "Households",
+        },
+      },
+    },
+  };
+});
+
 describe("App", () => {
   test("test if the login button exists", () => {
     render(<TestApp />);
