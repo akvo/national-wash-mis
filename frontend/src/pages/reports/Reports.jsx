146356b459613ab4217c2d4d4def79d08482872b
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-04-13 13:27:49 +0700

    [#19] Implementing i18n in reports page

--- frontend/src/pages/reports/Reports.jsx
+++ frontend/src/pages/reports/Reports.jsx
@@ -4,16 +4,18 @@ import { Row, Col, Card, Button } from "antd";
 import { store, config } from "../../lib";
 import { VisualisationFilters } from "../../components";
 import { Link } from "react-router-dom";
+import { getTranslation } from "../../util";
 
 const Reports = () => {
-  const { selectedForm } = store.useState((state) => state);
-
+  const { selectedForm, language } = store.useState((state) => state);
+  const { active: activeLang } = language;
+  const text = getTranslation(activeLang, "report");
   const filtered = config?.templates.filter((t) => t.formId === selectedForm);
 
   return (
     <div id="reports">
       <VisualisationFilters persist={true} />
-      <h2>Choose a template</h2>
+      <h2>{text.chooseTemplate}</h2>
       {filtered.length ? (
         <Row gutter={[16, 16]}>
           {filtered.map((t, tI) => (
@@ -22,14 +24,14 @@ const Reports = () => {
                 <h3>{t.name}</h3>
                 <h4>{t.title}</h4>
                 <Link to={`/report/${t.id}`}>
-                  <Button type="primary">Select</Button>
+                  <Button type="primary">{text.select}</Button>
                 </Link>
               </Card>
             </Col>
           ))}
         </Row>
       ) : (
-        <p className="text-muted">No templates found</p>
+        <p className="text-muted">{text.noTemplateFound}</p>
       )}
     </div>
   );
