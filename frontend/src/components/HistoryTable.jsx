b04017ca607524a7c57b418b42d69e9a8eef32b3
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-04-13 13:18:34 +0700

    [#19] Implementing i18n in table view components

--- frontend/src/components/HistoryTable.jsx
+++ frontend/src/components/HistoryTable.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Table } from "antd";
+import { store } from "../lib";
+import { getTranslation } from "../util";
 
 const HistoryTable = ({ record }) => {
   const { history, id } = record;
+  const { active: activeLang } = store.useState((s) => s.language);
+  const text = getTranslation(activeLang, "historyTable");
   return (
     <div className="history-table-wrapper">
       <Table
@@ -10,20 +14,20 @@ const HistoryTable = ({ record }) => {
         rowKey={`history-${id}-${Math.random}`}
         columns={[
           {
-            title: "History",
+            title: text.history,
             dataIndex: "value",
             key: "value",
             ellipsis: true,
           },
           {
-            title: "Updated at",
+            title: text.updatedAt,
             dataIndex: "created",
             key: "created",
             align: "center",
             ellipsis: true,
           },
           {
-            title: "Updated by",
+            title: text.updatedBy,
             dataIndex: "created_by",
             key: "created_by",
             align: "center",
