136a9a69d9648f3bb36484b3c6c23daede4f4fcc
dedenbangkit <mail@dedenbangkit.com>
2023-04-14 10:35:15 +0700

    Update apk packages

--- ci/test/entrypoint.sh
+++ ci/test/entrypoint.sh
@@ -6,9 +6,9 @@ apk --update add \
     --no-cache \
     --no-progress \
     bash~=5.1 \
-    curl~=8.0.1-r2 \
+    curl~=8.0.1-r0 \
     jq~=1.6 \
-    wait4ports=~0.3 \
-    postgresql-client
+    wait4ports=~0.3.3-r0 \
+    postgresql-client=13.10-r0
 
 exec "$@"
