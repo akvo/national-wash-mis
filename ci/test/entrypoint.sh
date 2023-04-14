4e83250c4992d46e079a76878d5362ea321973d7
dedenbangkit <mail@dedenbangkit.com>
2023-04-14 10:26:40 +0700

    Update alpine apk

--- ci/test/entrypoint.sh
+++ ci/test/entrypoint.sh
@@ -5,8 +5,8 @@ set -ex
 apk --update add \
     --no-cache \
     --no-progress \
-    bash~=5 \
-    curl~=7 \
+    bash~=5.1 \
+    curl~=8.0.1-r2 \
     jq~=1.6 \
     wait4ports=~0.3 \
     postgresql-client
