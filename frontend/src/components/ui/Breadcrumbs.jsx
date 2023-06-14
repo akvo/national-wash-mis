443b696640ffa579186b8c58b09311e5de4c741b
ifirmawan <firmawaneiwan@gmail.com>
2023-06-14 12:01:49 +0700

    [#94] Implement form title trans on breadcrumbs

--- frontend/src/components/ui/Breadcrumbs.jsx
+++ frontend/src/components/ui/Breadcrumbs.jsx
@@ -1,31 +1,48 @@
 import React from "react";
 import { Breadcrumb, Typography } from "antd";
 import { Link } from "react-router-dom";
+import kebabCase from "lodash/kebabCase";
 import "./style.scss";
-
+import { formTrans, store } from "../../lib";
 const { Title } = Typography;
 
 const Breadcrumbs = ({ pagePath }) => {
+  const { language } = store.useState((s) => s);
+  const { active: activeLang } = language;
+
   if (pagePath.length < 1) {
     return "";
   }
   return (
     <Breadcrumb separator=">">
-      {pagePath.map((path, pathIndex) => (
-        <Breadcrumb.Item key={pathIndex}>
-          {path.link ? (
-            <Link to={path.link}>
+      {pagePath.map((path, pathIndex) => {
+        const keyTrans = Object.keys(formTrans).find(
+          (k) => k === kebabCase(path.title)
+        );
+        let pathTitle = path.title;
+        if (
+          !path.link &&
+          formTrans[keyTrans] &&
+          formTrans[keyTrans][activeLang]
+        ) {
+          pathTitle = formTrans[keyTrans][activeLang];
+        }
+        return (
+          <Breadcrumb.Item key={pathIndex}>
+            {path.link ? (
+              <Link to={path.link}>
+                <Title style={{ display: "inline" }} level={pathIndex + 1}>
+                  {pathTitle}
+                </Title>
+              </Link>
+            ) : (
               <Title style={{ display: "inline" }} level={pathIndex + 1}>
-                {path.title}
+                {pathTitle}
               </Title>
-            </Link>
-          ) : (
-            <Title style={{ display: "inline" }} level={pathIndex + 1}>
-              {path.title}
-            </Title>
-          )}
-        </Breadcrumb.Item>
-      ))}
+            )}
+          </Breadcrumb.Item>
+        );
+      })}
     </Breadcrumb>
   );
 };
