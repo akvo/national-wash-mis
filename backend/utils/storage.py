94eccb26fdb70a2cb72b2bd33de46ed0b14a72c9
dedenbangkit <mail@dedenbangkit.com>
2023-08-15 10:58:17 +0700

    [#114] Modify docker compose storage

--- backend/utils/storage.py
+++ backend/utils/storage.py
@@ -1,14 +1,15 @@
 import os
+from nwmis.settings import STORAGE_PATH
 from pathlib import Path
 import shutil
 
 
 def upload(file: str, folder: str = None, filename: str = None):
-    storage_location = "./storage"
+    storage_location = STORAGE_PATH
     if folder:
         # create folder if not exists
-        Path(f"{storage_location}/{folder}").mkdir(parents=True, exist_ok=True)
-        storage_location = f"{storage_location}/{folder}"
+        Path(f"{STORAGE_PATH}/{folder}").mkdir(parents=True, exist_ok=True)
+        storage_location = f"{STORAGE_PATH}/{folder}"
     if not filename:
         filename = file.split("/")[-1]
     location = f"{storage_location}/{filename}"
@@ -17,12 +18,12 @@ def upload(file: str, folder: str = None, filename: str = None):
 
 
 def delete(url: str):
-    os.remove(f"./storage/{url}")
+    os.remove(f"{STORAGE_PATH}/{url}")
     return url
 
 
 def check(url: str):
-    path = Path(f"./storage/{url}")
+    path = Path(f"{STORAGE_PATH}/{url}")
     return path.is_file()
 
 
@@ -30,5 +31,5 @@ def download(url):
     # copy to tmp folder
     tmp_file = url.split("/")[-1]
     tmp_file = f"./tmp/{tmp_file}"
-    shutil.copy2(f"./storage/{url}", tmp_file)
-    return f"./storage/{url}"
+    shutil.copy2(f"{STORAGE_PATH}/{url}", tmp_file)
+    return f"{STORAGE_PATH}/{url}"
